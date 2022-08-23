a340abcc936294fccc174c12cd178d4610f81180
nimabht <nimabeheshtaein99@gmail.com>
2022-08-23 20:22:32 +0430

    making login form work

--- src/component/common/input.jsx
+++ src/component/common/input.jsx
@@ -2,15 +2,21 @@ import React, { Component } from "react";
 class Input extends Component {
   render() {
     return (
-      <div className="w-56 flex flex-col gap-y-1">
+      <div className="mb-5 w-56 flex flex-col gap-y-2">
         <label htmlFor={this.props.name}>{this.props.label}:</label>
         <input
+          name={this.props.name}
+          onChange={this.props.onChange}
+          value={this.props.value}
           style={{ transition: "0.3s" }}
           className="p-2 rounded focus:outline-none focus:border-sky-500	 focus:border-2"
           placeholder={this.props.label}
-          type={this.props.type}
+          type={this.props.name}
           id={this.props.name}
         />
+        {this.props.error && (
+          <p className="text-xs text-red-600	">{this.props.error}</p>
+        )}
       </div>
     );
   }
