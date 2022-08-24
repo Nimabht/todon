49d713e994e7f490b28601cad771f48c3594e6a6
nimabht <nimabeheshtaein99@gmail.com>
2022-08-24 17:38:43 +0430

    fixing tailwind conf

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      width: {
-        96: "96%",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
