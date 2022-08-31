90c372d17480234cec33649f3dd859354509cb59
nimabht <nimabeheshtaein99@gmail.com>
2022-08-31 20:06:05 +0430

    not found page  created

--- src/component/common/task.jsx
+++ src/component/common/task.jsx
@@ -7,14 +7,14 @@ import FavoriteButton from "./favorite";
 class Task extends Component {
   render() {
     return (
-      <div className="m-0 flex gap-x-4 items-center	">
-        <div className="m-0 min-h-[25%] h-shadow-xl items-center flex self-start bg-purple-100	h-fit w-[90%] rounded-2xl gap-x-12 overflow-hidden	">
-          <h1 className="p-2 text-3xl ml-9">{this.props.title}</h1>
+      <div className="m-0 flex gap-x-4 items-center">
+        <div className="m-0 h-[8rem] h-shadow-xl items-center flex self-start bg-purple-100	h-fit w-[90%] rounded-2xl gap-x-12 overflow-hidden	justify-between		">
+          <h1 className="p-2 text-3xl ml-9 w-[7rem]">{this.props.title}</h1>
           <p
             className={
               this.props.task.status
-                ? "p-3 h-fit w-3/5 line-through"
-                : "p-3 h-fit w-3/5"
+                ? "p-3 h-fit w-[37rem] line-through"
+                : "p-3 h-fit w-[37rem]"
             }
           >
             {this.props.description}
--- src/component/not-found.jsx
+++ src/component/not-found.jsx
@@ -1,7 +1,23 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const NotFound = () => {
-  return <h1>Not Found</h1>;
+  return (
+    <div id="notfound">
+      <div className="notfound">
+        <div className="notfound-404">
+          <h1>
+            4<span className="hover:m-[1rem] transition-all	"></span>4
+          </h1>
+        </div>
+        <h2>Oops! Page Not Be Found</h2>
+        <p>
+          Sorry but the page you are looking for does not exist, have been
+          removed. name changed or is temporarily unavailable
+        </p>
+        <Link to="/">Back to homepage</Link>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
